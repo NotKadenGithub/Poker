4c2a7337c2105c714f1ce68e3233b629b4b7defc
KadenT <65406345+NotKadenGithub@users.noreply.github.com>
2022-01-22 09:15:42 +0000

    Finished the image database and added a deck

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #imports
 
 import random
+from db import cardImages
 
 print("""
  /$$$$$$$           /$$                          
@@ -10,26 +11,14 @@ print("""
 | $$____/| $$  \ $$| $$$$$$/ | $$$$$$$$| $$  \__/
 | $$     | $$  | $$| $$_  $$ | $$_____/| $$      
 | $$     |  $$$$$$/| $$ \  $$|  $$$$$$$| $$      
-|__/      \______/ |__/  \__/ \_______/|__/      
-      
-      
-      """)
-
-
-#Cards
-cards = {
-  "spaAce": """   _____
-                 |A .  |
-                 | /.\ |
-                 |(_._)|
-                 |  |  |
-                 |____V|""",
-  "diaAce": ""
-}
+|__/      \______/ |__/  \__/ \_______/|__/
+""")
 
 
 
 
+
+#Tutorial
 
 print("""
 Do you know how to play poker?
@@ -38,6 +27,30 @@ Type "y" for yes and "n" for no
 
 choice = input("")
 choice = choice.lower()
-if choice == "n":
-  print()
+loopTutorial = True
+while loopTutorial == True:
+  if choice == "n":
+    print()
+    loopTutorial = False
+  elif choice == "y":
+    print()
+    loopTutorial = False
+  else:
+    print("Invalid Input")
+
+
+#Main code
+
+deck = ["spaAce","spa2","spa3","spa4","spa5","spa6","spa7","spa8","spa9","spa10","spaJ","spaQ","spaK","clubAce","club2","club3","club4","club5","club6","club7","club8","club9","club10","clubJ","clubK","clubQ","heaAce","hea2","hea3","hea4","hea5","hea6","hea7","hea8","hea9","hea10","heaJ","heaQ","heaK","diaAce","dia2","dia3","dia4","dia5","dia6","dia7","dia8","dia9","dia10","diaJ","diaQ","diaK"]
+
+usersHand = []
+
+pool = []
+
+
+def removeCards(amount, deckToAdd):
+  for i in range(0,amount):
+    randomCard = random.randint(0, len(deck) - 1)
+    deckToAdd.append(deck[randomCard])
+    del deck[randomCard]
 
